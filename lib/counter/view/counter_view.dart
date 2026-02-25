a7ce748cf4b947b8a340dbd9f55d0f9c46e38786
JoshRoderick18 <joshroderick18@gmail.com>
2026-02-25 11:13:47 -0300

    feat: add CounterView widget to display counter value and manage actions

---
+++ lib/counter/view/counter_view.dart
@@ -0,0 +1,37 @@
+import 'package:bloc_playground/counter/bloc/counter_bloc.dart';
+import 'package:bloc_playground/counter/bloc/counter_event.dart';
+import 'package:bloc_playground/counter/bloc/counter_state.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+class CounterView extends StatelessWidget {
+  const CounterView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('BLoC Counter')),
+      body: Center(
+        child: BlocBuilder<CounterBloc, CounterState>(
+          builder: (context, state) {
+            return Text('${state.counterValue}', style: Theme.of(context).textTheme.headlineMedium);
+          },
+        ),
+      ),
+      floatingActionButton: Column(
+        mainAxisAlignment: .end,
+        spacing: 8,
+        children: [
+          FloatingActionButton(
+            onPressed: () => context.read<CounterBloc>().add(CounterIncremented()),
+            child: const Icon(Icons.add),
+          ),
+          FloatingActionButton(
+            onPressed: () => context.read<CounterBloc>().add(CounterDecremented()),
+            child: const Icon(Icons.remove),
+          ),
+        ],
+      ),
+    );
+  }
+}
