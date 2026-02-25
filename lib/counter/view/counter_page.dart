00aa68caa6252a805538f88c345d43e87e986d01
JoshRoderick18 <joshroderick18@gmail.com>
2026-02-25 11:13:43 -0300

    feat: add CounterPage widget to provide CounterBloc and display CounterView

---
+++ lib/counter/view/counter_page.dart
@@ -0,0 +1,13 @@
+import 'package:bloc_playground/counter/bloc/counter_bloc.dart';
+import 'package:bloc_playground/counter/view/counter_view.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+class CounterPage extends StatelessWidget {
+  const CounterPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return BlocProvider(create: (context) => CounterBloc(), child: const CounterView());
+  }
+}
