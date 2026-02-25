7c75f0ffc1bc92fff2cf2a680f865fdfc061b92d
JoshRoderick18 <joshroderick18@gmail.com>
2026-02-25 11:13:27 -0300

    feat: implement CounterBloc to manage counter state and events

---
+++ lib/counter/bloc/counter_bloc.dart
@@ -0,0 +1,15 @@
+import 'package:bloc_playground/counter/bloc/counter_event.dart';
+import 'package:bloc_playground/counter/bloc/counter_state.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+class CounterBloc extends Bloc<CounterEvent, CounterState> {
+  CounterBloc() : super(const CounterState(counterValue: 0)) {
+    on<CounterIncremented>((event, emit) {
+      emit(CounterState(counterValue: state.counterValue + 1));
+    });
+
+    on<CounterDecremented>((event, emit) {
+      emit(CounterState(counterValue: state.counterValue - 1));
+    });
+  }
+}
