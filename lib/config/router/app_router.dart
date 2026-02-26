3c7ae54632658d5d3a3ca93c7145bdc370a40122
JoshRoderick18 <joshroderick18@gmail.com>
2026-02-26 14:50:46 -0300

    feat: add app router with initial routes for HomeScreen

---
+++ lib/config/router/app_router.dart
@@ -0,0 +1,9 @@
+import 'package:bloc_playground/presentation/screens/screens.dart';
+import 'package:go_router/go_router.dart';
+
+final appRouter = GoRouter(
+  routes: [
+    GoRoute(path: '/', builder: (context, state) => HomeScreen()),
+    GoRoute(path: '/cubits', builder: (context, state) => HomeScreen()),
+  ],
+);
